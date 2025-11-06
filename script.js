673ee9cefa09d4e906ae633140e28dd6a31e6f8e
tunaflsh <tunaflish.ai@gmail.com>
2025-11-06 18:04:05 +0100

    Replace event duration with the first Monday of the next month
    Replaced getSecondFriday with a more flexible getNthWeekday function to calculate event dates. Updated event start and end calculations accordingly.

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const countdownElement = document.getElementById("countdown");
 
 
-function getSecondFriday(year, month) {
-  const weekday = new Date(`${year}-${month}-01`).getDay();
-  const secondFriday = String(1 + 7 + (5 - weekday + 7) % 7).padStart(2, "0");
+/**
+ * Returns nth weekday of the month. Example:
+ * `getNthWeekday(2, 2025, 10, 5, 17)`
+ * will return the second Friday of October in 2025, at 17:00, October 10, 2025.
+ *
+ * @param {number} n the order of the weekday in the month
+ * @param {number} year
+ * @param {number} month the month number starts from 1 in January
+ * @param {number} weekday the weekday number starts from 0 on Sunday
+ * @param {number} [hour=0] the hour of the day in UTC (24h format)
+ * @param {number} [minute=0] the minute of the hour
+ * @param {number} [second=0] the second of the minute
+ * @return {Date}
+ */
+function getNthWeekday(n, year, month, weekday, hour=0, minute=0, second=0) {
+  const firstWeekday = new Date(`${year}-${month}-01`).getDay();
+  const nthWeekday = String(1 + 7*(n-1) + (weekday - firstWeekday + 7) % 7).padStart(2, "0");
+  hour = String(hour).padStart(2, "0");
+  minute = String(minute).padStart(2, "0");
+  second = String(second).padStart(2, "0");
 
-  return new Date(`${year}-${month}-${secondFriday}T17:00:00Z`);  // second Friday of the month
+  return new Date(`${year}-${month}-${nthWeekday}T${hour}:${minite}:${second}Z`);  // nth weekday of the month
 }
 
 
@@ -21,12 +38,10 @@ function getDDHHMMSS(time) {
 
 function update() {
   const now = new Date();
-  const month = 10;  // Banish the Beyond starts in October
   const year = now.getUTCFullYear();
-  let eventStart = getSecondFriday(year, month);
+  let eventStart = getNthWeekday(2, year, 10, 5);  // second Friday of October
 
-  const eventDuration = 2030400000;  // 23d 12h (in milliseconds)
-  const eventEnd = new Date(eventStart.getTime() + eventDuration);
+  const eventEnd = getNthWeekday(1, year, 11, 1);  // first Monday of November
 
   if (eventStart <= now && now < eventEnd) {  // The event is ongoing
     countdownElement.innerHTML = getDDHHMMSS(eventEnd - now) + '<br>left to banish them beyond';
@@ -34,7 +49,7 @@ function update() {
   }
 
   if (eventEnd <= now) {  // the next event will start next year
-    eventStart = getSecondFriday(year + 1, month);
+    eventStart = getNthWeekday(2, year + 1, 10, 5);
   }
 
   countdownElement.innerHTML = getDDHHMMSS(eventStart - now) + '<br>until the spirit is released';
